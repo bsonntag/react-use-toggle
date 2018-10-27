74fa58590829f1a5c96f35cf1608932df747785b
Benjamim Sonntag <benjamimsonntag@gmail.com>
2018-10-27 13:50:06 +0100

    Add example

---
+++ example/index.js
@@ -0,0 +1,21 @@
+import { render } from 'react-dom';
+import React from 'react';
+import useToggle from '../src';
+
+const Example = () => {
+  const [on, toggle] = useToggle(false);
+  const [cool, toggleCool] = useToggle(true);
+
+  return (
+    <>
+      <button onClick={toggle}>
+        {on ? 'On' : 'Off'}
+      </button>
+      <button onClick={toggleCool}>
+        {cool ? 'Cool' : 'Meh'}
+      </button>
+    </>
+  );
+};
+
+render(<Example />, document.getElementById('root'));
